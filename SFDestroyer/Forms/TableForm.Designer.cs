c3fd09564a049254b955337e4ac3e64776c3ff16
DorTem <mister-evseeff.2011@mail.ru>
2020-04-07 01:26:38 +0300

    SFDestroyer 1.0

--- SFDestroyer/Forms/TableForm.Designer.cs
+++ SFDestroyer/Forms/TableForm.Designer.cs
@@ -34,6 +34,7 @@
             this.fileNames = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.but_Del = new System.Windows.Forms.Button();
             this.panel_Upper.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,8 +44,9 @@
             this.panel_Upper.Controls.Add(this.label_CloseWindow);
             this.panel_Upper.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel_Upper.Location = new System.Drawing.Point(0, 0);
+            this.panel_Upper.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel_Upper.Name = "panel_Upper";
-            this.panel_Upper.Size = new System.Drawing.Size(676, 18);
+            this.panel_Upper.Size = new System.Drawing.Size(789, 22);
             this.panel_Upper.TabIndex = 3;
             this.panel_Upper.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_Upper_MouseDown);
             this.panel_Upper.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_Upper_MouseMove);
@@ -55,7 +57,7 @@
             this.label_CloseWindow.AutoSize = true;
             this.label_CloseWindow.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label_CloseWindow.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label_CloseWindow.Location = new System.Drawing.Point(657, 2);
+            this.label_CloseWindow.Location = new System.Drawing.Point(766, 2);
             this.label_CloseWindow.Name = "label_CloseWindow";
             this.label_CloseWindow.Size = new System.Drawing.Size(16, 16);
             this.label_CloseWindow.TabIndex = 0;
@@ -73,23 +75,25 @@
             this.list_NT.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.list_NT.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.list_NT.HideSelection = false;
-            this.list_NT.Location = new System.Drawing.Point(276, 40);
+            this.list_NT.Location = new System.Drawing.Point(333, 41);
+            this.list_NT.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.list_NT.Name = "list_NT";
-            this.list_NT.Size = new System.Drawing.Size(381, 514);
+            this.list_NT.Size = new System.Drawing.Size(444, 640);
             this.list_NT.TabIndex = 6;
             this.list_NT.UseCompatibleStateImageBehavior = false;
             this.list_NT.View = System.Windows.Forms.View.Details;
+            this.list_NT.DoubleClick += new System.EventHandler(this.list_NT_DoubleClick);
             // 
             // fileNames
             // 
             this.fileNames.Tag = "fff";
             this.fileNames.Text = "Name";
-            this.fileNames.Width = 319;
+            this.fileNames.Width = 353;
             // 
             // Type
             // 
             this.Type.Text = "Type";
-            this.Type.Width = 61;
+            this.Type.Width = 90;
             // 
             // listBox1
             // 
@@ -97,21 +101,40 @@
             this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.listBox1.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 71);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 87);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(258, 483);
+            this.listBox1.Size = new System.Drawing.Size(315, 594);
             this.listBox1.TabIndex = 7;
+            // 
+            // but_Del
+            // 
+            this.but_Del.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.but_Del.FlatAppearance.BorderSize = 0;
+            this.but_Del.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but_Del.Location = new System.Drawing.Point(690, 689);
+            this.but_Del.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.but_Del.Name = "but_Del";
+            this.but_Del.Size = new System.Drawing.Size(87, 28);
+            this.but_Del.TabIndex = 8;
+            this.but_Del.Text = "Delete";
+            this.but_Del.UseVisualStyleBackColor = false;
+            this.but_Del.Click += new System.EventHandler(this.but_Del_Click);
             // 
             // TableForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.ClientSize = new System.Drawing.Size(676, 592);
+            this.ClientSize = new System.Drawing.Size(789, 729);
+            this.Controls.Add(this.but_Del);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.list_NT);
             this.Controls.Add(this.panel_Upper);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "TableForm";
             this.Text = "TableForm";
             this.panel_Upper.ResumeLayout(false);
@@ -128,5 +151,6 @@
         private System.Windows.Forms.ColumnHeader fileNames;
         private System.Windows.Forms.ColumnHeader Type;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button but_Del;
     }
 }
--- SFDestroyer/Forms/TableForm.cs
+++ SFDestroyer/Forms/TableForm.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using SFDestroyer.Forms;
 using System.IO;
+using System.Diagnostics;
+using System.Globalization;
+
+
 
 namespace SFDestroyer.Forms
 {
@@ -56,13 +58,14 @@ namespace SFDestroyer.Forms
 
             foreach(string files in allFiles)
             {
-                list_NT.Items.Add(files).SubItems.Add(Path.GetExtension(files));
+                list_NT.Items.Add(Path.GetFileName(files)).SubItems.Add(Path.GetExtension(files));
             }
             
             void Scanner(string mainPath)
             {
                 //array = files and subdirs in current directory [path]
                 string[] files_path = Directory.GetFileSystemEntries(mainPath);
+                int file_count = 0;
                 try
                 {
                     foreach (string file in files_path)
@@ -77,6 +80,7 @@ namespace SFDestroyer.Forms
                                 allFiles.Add(file);
                             }
                         }
+                        //if dir
                         else if (Directory.Exists(file))
                         {
                             Scanner(file);
@@ -95,8 +99,37 @@ namespace SFDestroyer.Forms
         {
             Close();
         }
-       
 
+        private void list_NT_DoubleClick(object sender, EventArgs e)
+        {
 
+            string textfile = allFiles[list_NT.SelectedIndices[0]];
+            int position = 0;
+
+            // Find position of last "\"
+            for (int counter = textfile.Length - 1; counter != 0; counter--)
+            {
+                if (textfile[counter].ToString(CultureInfo.InvariantCulture) == "\\")
+                {
+
+                    position = counter;
+                    break;
+                }
+            }
+            //Start explorer.exe
+            Process.Start(new ProcessStartInfo("explorer.exe", " /select, " + textfile));
+        }
+
+        private void but_Del_Click(object sender, EventArgs e)
+        {
+            File.Delete(allFiles[list_NT.SelectedIndices[0]]);
+            allFiles.RemoveAt(list_NT.SelectedIndices[0]);
+            list_NT.Items.Clear();
+
+            foreach (string file in allFiles)
+            {
+                list_NT.Items.Add(Path.GetFileName(file)).SubItems.Add(Path.GetExtension(file));
+            }
+        }
     }
 }
